df30f2c05af1434e18300ab2ca40e60c7ae5120c
pada0709 <pada0709@naver.com>
2021-08-21 16:42:23 +0900

    #6.1 Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import {HashRouter, Router} from 'react-router-dom'
+import Home from "./routes/Home"
+import About from "./routes/About"
+import { Route } from 'react-router'
 
 function App() {
-  return <span>something</span>
+  return (
+    <HashRouter>
+      <Route path="\" exact={true} component={Home} />
+      <Route path="\about" component={About} />
+    </HashRouter>
+  )
 }
 
 export default App;
